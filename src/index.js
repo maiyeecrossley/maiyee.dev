df3d4e5d18065b48513f24d9e1413b671286d016
maiyeecrossley <maiyee.crossley@gmail.com>
2025-03-23 21:28:01 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.js'
+import { BrowserRouter } from 'react-router'
+import { HelmetProvider } from 'react-helmet-async'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <HelmetProvider>
     <App />
-  </React.StrictMode>
-);
+    </HelmetProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
